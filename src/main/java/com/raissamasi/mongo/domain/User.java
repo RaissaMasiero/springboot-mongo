4157dbfa69f6ec8553f7be9fa119f8196130816a
Raissa Masiero <raissamasi2@gmail.com>
2023-05-02 17:21:11 -0300

    Entidade User e Rest funcionando

---
+++ src/main/java/com/raissamasi/mongo/domain/User.java
@@ -0,0 +1,56 @@
+package com.raissamasi.mongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private String id;
+    private String nome;
+    private String email;
+
+    public User(){}
+
+    public User(String id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
