e21644d069d533df57ae0b9463bf9f1822535915
Raissa Masiero <raissamasi2@gmail.com>
2023-05-02 19:29:48 -0300

    Conectando ao mongoDB com repository e service

---
+++ src/main/java/com/raissamasi/mongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.raissamasi.mongo.repository;
+
+import com.raissamasi.mongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/raissamasi/mongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.raissamasi.mongo.services;
+
+import com.raissamasi.mongo.domain.User;
+import com.raissamasi.mongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+}
