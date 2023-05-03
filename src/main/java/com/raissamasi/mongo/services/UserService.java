c97b616030b8f2a97fc10be7e05e0c26587b6941
Raissa Masiero <raissamasi2@gmail.com>
2023-05-03 15:23:43 -0300

    Inserção de usuário com Post

--- src/main/java/com/raissamasi/mongo/services/UserService.java
+++ src/main/java/com/raissamasi/mongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.raissamasi.mongo.services;
 
 import com.raissamasi.mongo.domain.User;
+import com.raissamasi.mongo.dto.UserDTO;
 import com.raissamasi.mongo.repository.UserRepository;
 import com.raissamasi.mongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getNome(), objDTO.getEmail());
     }
 }
