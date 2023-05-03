be34ca097c0c3ab3dcfb59aaa62b56af60532d0b
Raissa Masiero <raissamasi2@gmail.com>
2023-05-03 15:08:42 -0300

    Obtendo um usuário por Id

---
+++ src/main/java/com/raissamasi/mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.raissamasi.mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
