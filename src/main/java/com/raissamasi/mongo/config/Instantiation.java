6579bd507e23c806da47dab5c97e036bd7717b78
Raissa Masiero <raissamasi2@gmail.com>
2023-05-03 14:05:59 -0300

    Instanciação da base de dados

---
+++ src/main/java/com/raissamasi/mongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.raissamasi.mongo.config;
+
+import com.raissamasi.mongo.domain.User;
+import com.raissamasi.mongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll(); // limpa as inserções no mongo db
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
